f90eb0e2e2ab1ad2ba672ef61523118b83910eca
Obrienmaina <brianmaina.nyawira@gmail.com>
2025-10-09 09:53:45 +0200

    update

--- portfolio/src/lib/mongodb.ts
+++ portfolio/src/lib/mongodb.ts
@@ -13,7 +13,7 @@ let clientPromise: Promise<MongoClient>
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof global & {
+  const globalWithMongo = global as typeof global & {
     _mongoClientPromise?: Promise<MongoClient>
   }
 
@@ -28,6 +28,5 @@ if (process.env.NODE_ENV === 'development') {
   clientPromise = client.connect()
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
 export default clientPromise
+
