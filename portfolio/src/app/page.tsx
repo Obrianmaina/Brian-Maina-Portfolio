14cf8f2210b169c9a64ac6d6e354e47cb7f8a51b
Obrienmaina <brianmaina.nyawira@gmail.com>
2025-10-23 15:02:48 +0200

    Coorprate work

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -185,7 +185,7 @@ export default function PortfolioPage() {
   ];
 
   // UPDATED: New skills array
-  const skills = ["UI/UX Design", "Graphic Design", "Motion Graphics", "Adobe Photoshop", "Adobe Illustrator", "Adobe After Effects", "Adobe Premier Pro", "Canva", "Adobe InDesign", "Adobe Audition", "Figma", "Microsoft Powerpoint", "Google Slides", "Data Visualization", "HTML", "CSS", "Davinci Resolve", "Github", "AI"];
+  const skills = ["UI/UX Design", "Graphic Design", "Motion Graphics", "Email Marketing", "Copy Writing", "Adobe Photoshop", "Adobe Illustrator", "Adobe After Effects", "Adobe Premier Pro", "Canva", "Adobe InDesign", "Adobe Audition", "Figma", "Microsoft Powerpoint", "Google Slides", "Data Visualization", "Mailchimp", "Mural", "Figjam", "Miro", "Google Slides", "HTML", "CSS", "Davinci Resolve", "Github", "VS Code", "Pycharm", "IntelliJ IDEA", "Corel Draw", "AI"];
 
   const showcases: Showcase[] = [
     
@@ -616,30 +616,34 @@ export default function PortfolioPage() {
 
   const companyProjects: CompanyProject[] = [
     {
-      companyName: "Company I worked for",
-      companyLogo: "/images/logos/sap-logo.svg",
-      disclaimer: "The following work was created during my tenure at Company. It is shared with permission for portfolio purposes only and remains the intellectual property of Company. The content is confidential and should not be distributed, copied, or disclosed.",
+      companyName: "Aspira Kenya",
+      companyLogo: "https://raw.githubusercontent.com/Obrianmaina/Brian-Maina-Portfolio/main/portfolio/public/images/corporate/Aspira_Logo-1.svg",
+      disclaimer: "The following work was created during my tenure at Aspira Kenya. It is shared with permission for portfolio purposes only and remains the intellectual property of Aspira Kenya. The content is confidential and should not be distributed, copied, or disclosed.",
       projects: [ {
-          title: "Confidential Project",
+          title: "Omoka na Aspira Marketing Campaign Visual",
           category: "Graphics",
-          description: "Still awaiting permission to showcase this projects.",
+          description: "Creating Omoka na Aspira visual, including lockup and social media banners for Aspira's marketing campaign.",
           tag: "Corporate Work",
           mediaType: "image",
-          media: "https://raw.githubusercontent.com/Obrianmaina/Brian-Maina-Portfolio/main/portfolio/public/images/Approval/Awaiting%20Consent.png",
-          challenge: "Will update upon receiving permission.",
-          process: "Will update upon receiving permission.",
-          outcome: "Will update upon receiving permission."
+          media: ["https://raw.githubusercontent.com/Obrianmaina/Brian-Maina-Portfolio/main/portfolio/public/images/corporate/Aspira/476121081_1070329765122570_6522906507805961232_n.jpg",
+          "https://raw.githubusercontent.com/Obrianmaina/Brian-Maina-Portfolio/main/portfolio/public/images/corporate/Aspira/476615140_1075259464629600_5660429119889293216_n.jpg",
+          ],
+          challenge: "Create a visual that clearly emphasized that Aspira will help you finance your dreams that you may pay later. Allowing customers to get what they want now and pay later.",
+          process: "Understanding and conducting audience research, creating a moodboard, working with the Marketing Lead: Arnold Muthama, refining concepts and sending for approval.",
+          outcome: "Creation of a harmonized campaign visual that spanned three months, used in marketing assets such as social media artwork, catalogues, email signitures and email campaigns."
       },
       {
-          title: "Confidential Project",
+          title: "Soma Education Financing Campaign Visuals for Aspira Kenya",
           category: "Graphics",
-          description: "Still awaiting permission to showcase this projects.",
+          description: "Creating social media artwork for Soma Education Financing, including the product's lockup.",
           tag: "Corporate Work",
           mediaType: "image",
-          media: "https://raw.githubusercontent.com/Obrianmaina/Brian-Maina-Portfolio/main/portfolio/public/images/Approval/Awaiting%20Consent.png",
-          challenge: "Will update upon receiving permission.",
-          process: "Will update upon receiving permission.",
-          outcome: "Will update upon receiving permission."
+          media: ["https://raw.githubusercontent.com/Obrianmaina/Brian-Maina-Portfolio/main/portfolio/public/images/corporate/Aspira/480434558_1082669703888576_6404166143007094392_n.jpg",
+          "https://raw.githubusercontent.com/Obrianmaina/Brian-Maina-Portfolio/main/portfolio/public/images/corporate/Aspira/481247611_1087361026752777_8786450958040780973_n.jpg",
+          ],
+          challenge: "Create social media artwork for Aspira's Facebook and Instagram pages that will help create awareness of Aspira's Soma Education financing, allowing back to school to be a breeze for parents as they can settle fees now and pay later.",
+          process: "Understanding and conducting audience research, creating a moodboard, working with the Marketing Lead: Arnold Muthama, refining concepts and sending for approval.",
+          outcome: "Creation of social media banners that enhanced the product visibilty to the online audience of parents during the back to school period."
       }
      ]
     },
