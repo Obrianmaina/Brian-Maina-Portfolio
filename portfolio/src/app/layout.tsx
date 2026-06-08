88e5c51c5385ada1154961d224573cff1e549ed3
Obrienmaina <brianmaina.nyawira@gmail.com>
2026-06-08 20:47:21 +0300

    no cookie banner

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
 
           <Footer />
           
-          <CookieBanner />
+          {/* <CookieBanner /> */}
         </ThemeProvider>
       </body>
     </html>
